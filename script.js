360e287316bfe1a6dc75908d744aa952f079ec49
Sarah Vivoli <snvivoli@gmail.com>
2019-11-16 16:00:31 -0800

    got answer buttons working

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var score = $("#score");
 var checkAnsDiv = $(".checkAnswer");
 var questionTag = $(".question");
 
-var secondsLeft = 75;
+var secondsLeft = 95;
 
 timer.text("Time: " + secondsLeft);
 
@@ -28,26 +28,35 @@ function setTime() {
   }, 1000);
 }
 
+var questionDiv = $("<div>");
+var questionTitle = $("<h2>");
+var answer1 = $("<button class='answer'>");
+var answer2 = $("<button class='answer'>");
+var answer3 = $("<button class='answer'>");
+var answer4 = $("<button class='answer'>");
+
 startButton.on("click", function (event) {
   event.preventDefault();
   setTime();
-  var questionDiv = $("<div>");
-  questionDiv.html("<h2 class=nextQuestion>" + "What group of tags are used to define the text headers in the body of the HTML document?" + "</h2>" + "<br>" + "<button class=ans1>" + "a. td" + "</button>" + "<br>" + "<button class=ans2>" + "b. h1 to h6" + "</button>" + "<br>" + "<button class=ans3>" + "c. button" + "</button>" + "<br>" + "<button class=ans4>" + "d. footer" + "</button>" + "<br>");
+  questionTitle.text("What group of tags are used to define the text headers in the body of the HTML document?");
+  questionDiv.append(questionTitle);
+  answer1.text("a. td");
+  questionDiv.append(answer1);
+  questionDiv.append("<br>");
+  answer2.text("b. h1 to h6");
+  questionDiv.append(answer2);
+  questionDiv.append("<br>");
+  answer3.text("c. button");
+  questionDiv.append(answer3);
+  questionDiv.append("<br>");
+  answer4.text("d. footer");
+  questionDiv.append(answer4);
+  questionDiv.append("<br>");
+  
   containerDiv.attr("style", "text-align: left");
   contentDiv.html(questionDiv.html());
 });
 
-// var answerClick = function() {
-//   checkAnsDiv.setAttribute("style", "display: block");
-//   contentDiv.innerHTML = "this is question 2";
-//   // for (var i = 0; i < questions.length; i++) {
-//   //   h1Tag.textContent = "hi";
-//   //   ansButton1.value = "a" + questions.i[choices[0]];
-//   //   ansButton2.value = "b" + questions.i[choices[1]];
-//   //   ansButton3.value = "c" + questions.i[choices[2]];
-//   //   ansButton4.value = "d" + questions.i[choices[3]];
-//   // }
-// };
 
 var questions = [
   {
@@ -61,29 +70,9 @@ var questions = [
     answer: "parentheses"
   },
   {
-    title: "What tag is used to define an image-or add an image- to an HTML page?",
-    choices: ["<table>", "<meta>", "<div>", "<img>"],
-    answer: "<img>"
-  },
-  {
-    title: "What tag is used to underline a word of line of text?",
-    choices: ["<li>", "<s>", "<u>", "<ul>"],
-    answer: "<ul>"
-  },
-  {
-    title: "What tag is required in all HTML documents, and is used to define the title?",
-    choices: ["<head>", "<body>", "<br>", "<title>"],
-    answer: "<title>"
-  },
-  {
     title: "What tag defines a devision or the beginning/end of an individual section in an HTML document?",
     choices: ["<br>", "<div>", "<table>", "<meta>"],
     answer: "<div>"
-  },
-  {
-    title: "What tag is used to define-and place-an interactive button in an HTML document?",
-    choices: ["<src>", "<button>", "<td>", "<clickfield>"],
-    answer: "<button>"
   },
   {
     title: "What tag defines the body of the HTML document, and usually includes all of the contents such as the text, hyperlinks, images, tables, lists, and more?",
@@ -91,119 +80,203 @@ var questions = [
     answer: "<body>"
   },
   {
-    title: "what tag is used to tdefine a table or image notation (caption)?",
-    choices: ["<!DOCTYPE>", "<embed>", "<code>", "<caption>"],
-    answer: "<caption>"
-  },
-  {
     title: "What is the name of the stylesheet that defines the presentation of an HTML of XML document?",
     choices: ["Java", "jQuery", "CSS", "PHP"],
     answer: "CSS"
-  },
-  {
-    title: "What is the most important CSS property, used for controlling the layout?",
-    choices: ["Margin", "Display", "<div>", "Table"],
-    answer: "Display"
   },
   {
     title: "The link element must go inside the ___ section of the HTML document or page.",
     choices: ["Footer", "Body", "Paragraph", "Head"],
     answer: "Head"
   },
-
-
+  {
+    title: "What is the name of the property that is used to define the special state of an element?",
+    choices: ["Pseudo-class", "Syntax", "Alignment", "Style"],
+    answer: "Pseudo-class"
+  },
+  {
+    title: "What is the box called in CSS that wraps around every HTML element?",
+    choices: ["Float", "Boundary", "Wrap", "Box-model"],
+    answer: "Box-model"
+  },
+  {
+    title: "What are the CSS properties that are used to ass space around sections of content?",
+    choices: ["Cleaner", "Padding", "Break", "Spacing"],
+    answer: "Padding"
+  },
+  {
+    title: "What is the name given to the CSS element that starts on the same line and only takes up as much width (space) as necessary?",
+    choices: ["Block-level", "Line", "Inline", "Spacer"],
+    answer: "Inline"
+  },
+  {
+    title: "What is the value called that defines colors such as the following: #FFFF00?",
+    choices: ["RGB Value", "Decimal Value", "Color Value", "Hex Value"],
+    answer: "Hex Value"
+  },
+  {
+    title: "What is the name of the statement that is used to exit or end a loop?",
+    choices: ["Falter statement", "Break statement", "Conditional statement", "Close statement"],
+    answer: "Break statement"
+  },
+  {
+    title: "What is the default behavior called that is used to move declarations to the top of the current scope?",
+    choices: ["Hoisting", "Jumping", "Sorting", "Arranging"],
+    answer: "Hoisting"
+  },
+  {
+    title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+    choices: ["Condtional loop", "Else loop", "While loop", "For loop"],
+    answer: "While loop"
+  },
+  {
+    title: "What is the format called that is used for storing and transporting data?",
+    choices: ["HTML", "Font", "Syntax", "JSON"],
+    answer: "JSON"
+  },
+  {
+    title: "What can loops offer JavaScript code as a whole?",
+    choices: ["Cross-platform support.", "Cleaner syntax.", "Added plug-ins.", "Improved performance."],
+    answer: "Improved performance."
+  },
+  {
+    title: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+    choices: ["Recorders", "Variables", "Arrays", "Strings"],
+    answer: "Strings"
+  },
+  {
+    title: "What is a Javascript element that represents either TRUE or FALSE?",
+    choices: ["Variable", "Event", "Condition", "Boolean"],
+    answer: "Boolean"
+  },
+  {
+    title: "A loop that never ends is referred to as a(n) ___.",
+    choices: ["While loop", "Infinite loop", "Recursive loop", "For loop"],
+    answer: "Infinite loop"
+  },
+  {
+    title: "Jay is considering adding a repetition statement within his Java programming final project. He is unsure of the number of times each loop needs to execute. Which of the statements below best fits the need identified by Jay within his programming?",
+    choices: ["While loop", "If-else", "For loop", "Switch statement"],
+    answer: "While loop"
+  },
+  {
+    title: "___ is the process of finding errors and fixing them within a program.",
+    choices: ["Compiling", "Executing", "Debugging", "Scanning"],
+    answer: "Debugging"
+  },
+  {
+    title: "Which of the following is not a required part of a for loop?",
+    choices: ["Iterator", "Condition", "String", "Increment"],
+    answer: "String"
+  },
+  {
+    title: "Where are global variables defined?",
+    choices: ["In the HTML document", "In the CSS document", "Outside of functions", "Inside of functions"],
+    answer: "Outside of functions"
+  },
+  {
+    title: "Which is the proper way to reference a class in the CSS document?",
+    choices: ["#className {}", "className {}", ".className {}", "@className {}"],
+    answer: ".className {}"
+  },
+  {
+    title: "Which command is used to push changes to GitHub from terminal?",
+    choices: ["git pwd", "git push add", "git init", "git push origin master"],
+    answer: "git push origin master"
+  },
+  {
+    title: "Which of the following DOES require an end tag (IS NOT an empty element)?",
+    choices: ["<img>", "<html>", "<br>", "<meta>"],
+    answer: "<html>"
+  },
+  {
+    title: "Which is the proper div class for a bootstrap element that spans 12 columns on a medium display?",
+    choices: ["column-md-12", "column-12-md", "col-md-12", "col-12-md"],
+    asnwer: "col-md-12"
+  },
+  {
+    title: "Which of the following will display a box with a message and an okay button only in the browser?",
+    choices: ["confirm()", "alert()", "prompt()", "None of the above"],
+    answer: "confirm()"
+  },
+  {
+    title: "Under which index is the string 'yellow' in the following array? var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']",
+    choices: ["0", "1", "2", "3"],
+    answer: "2"
+  },
+  {
+    title: "Which of the following is the iterator in the following for loop? for (var i=0; i<array.length; i++) {}",
+    choices: ["for", "i=0", "i<array.length", "i++"],
+    answer: "i=0"
+  }
 ];
 
+var newDiv = $("<div>");
+var newTitle = $("<h2>");
+var newanswer1 = $("<button class='answer'>");
+var newanswer2 = $("<button class='answer'>");
+var newanswer3 = $("<button class='answer'>");
+var newanswer4 = $("<button class='answer'>");
+
+$(contentDiv).on("click", ".answer", function(event) {
+    event.preventDefault();
+    newTitle.text(questions[0].title);
+    newDiv.append(newTitle);
+    newanswer1.text("a. " + questions[0].choices[0]);
+    newDiv.append(newanswer1);
+    newDiv.append("<br>");
+    newanswer2.text("b. " + questions[0].choices[1]);
+    newDiv.append(newanswer2);
+    newDiv.append("<br>");
+    newanswer3.text("c. " + questions[0].choices[2]);
+    newDiv.append(newanswer3);
+    newDiv.append("<br>");
+    newanswer4.text("d. " + questions[0].choices[3]);
+    newDiv.append(newanswer4);
+    newDiv.append("<br>");
+    contentDiv.html(newDiv.html());
+
+    // var checkAns = $("<div>");
+    // if (answer1.html() === questions[0].answer[0]) {
+    //   checkAns.html("<p>Correct!</p>");
+    // } else {
+    //   checkAns.html("<p>Wrong!</p>");
+    //   secondsLeft -10;
+    // };
+    // checkAns.attr("style", "disply: block");
+    // questionDiv.append(checkAns);
+
+});
 
 
-var ans1 = $(".ans1");
-var ans2 = $(".ans2");
-var ans3 = $(".ans3");
-var ans4 = $(".ans4");
-var nextQuestion = $(".nextQuestion")
-// // var ansButtons = $(".ansButtons");
-
-// for (var i=0; i<questions.length; i++) {
-//   ans1.on("click", function(event) {
+// for (var i = 0; i < questions.length; i++) {
+//   answer.on("click", function(event) {
 //     event.preventDefault();
-//     var newQuesDiv = $("<div>");
-//     newQuesDiv.html("<h1>" + questions[i].title + "<h1>" + "<button>" + questions[i].choices[0] + "<br>" + "<button>" + questions[i].choices[1] + "<br>" + "<button>" + questions[i].choices[2] + "<br>" + "<button>" + questions[i].choices[3] + "<br>");
-//     newQuesDiv.html(newQuesDiv.html());
+//     newTitle.text(questions[i].title);
+//     newDiv.append(newTitle);
+//     newanswer1.text("a" + questions[i].choices[0]);
+//     newDiv.append(newanswer1);
+//     newDiv.append("<br>");
+//     newanswer2.text("b" + questions[i].choices[1]);
+//     newDiv.append(newanswer2);
+//     newDiv.append("<br>");
+//     newanswer3.text("c" + questions[i].choices[2]);
+//     newDiv.append(newanswer3);
+//     newDiv.append("<br>");
+//     newanswer4.text("d" + questions[i].choices[3]);
+//     newDiv.append(newanswer4);
+//     newDiv.append("<br>");
+
+
+//     var checkAns = $("<div>");
+//     if (ans1 === answer) {
+//       checkAns.html("<p>Correct!</p>");
+//     } else {
+//       checkAns.html("<p>Wrong!</p>");
+//     };
+//     questionDiv.append(checkAns);
 //   })
-// }
-
-for (var i = 0; i < questions.length; i++) {
-  ans1.on("click", function(event) {
-    event.preventDefault();
-    nextQuestion.html(questions[i].title);
-    ans1.html(questions[i].choices[0]);
-    ans2.html(questions[i].choices[1]);
-    ans3.html(questions[i].choices[2]);
-    ans4.html(questions[i].choices[3]);
-
-    var checkAns = $("<div>");
-    if (ans1 === answer) {
-      checkAns.html("<p>Correct!</p>");
-    } else {
-      checkAns.html("<p>Wrong!</p>");
-    };
-    questionDiv.append(checkAns);
-  })
-};
-
-for (var i = 0; i < questions.length; i++) {
-  ans2.on("click", function(event) {
-    event.preventDefault();
-    nextQuestion.html(questions[i].title);
-    ans1.html(questions[i].choices[0]);
-    ans2.html(questions[i].choices[1]);
-    ans3.html(questions[i].choices[2]);
-    ans4.html(questions[i].choices[3]);
-
-    var checkAns = $("<div>");
-    if (ans2 === answer) {
-      checkAns.html("<p>Correct!</p>");
-    } else {
-      checkAns.html("<p>Wrong!</p>");
-    };
-  })
-};
-
-for (var i = 0; i < questions.length; i++) {
-  ans3.on("click", function(event) {
-    event.preventDefault();
-    nextQuestion.html(questions[i].title);
-    ans1.html(questions[i].choices[0]);
-    ans2.html(questions[i].choices[1]);
-    ans3.html(questions[i].choices[2]);
-    ans4.html(questions[i].choices[3]);
-
-    var checkAns = $("<div>");
-    if (ans3 === answer) {
-      checkAns.html("<p>Correct!</p>");
-    } else {
-      checkAns.html("<p>Wrong!</p>");
-    };
-  })
-};
-
-for (var i = 0; i < questions.length; i++) {
-  ans4.on("click", function(event) {
-    event.preventDefault();
-    nextQuestion.html(questions[i].title);
-    ans1.html(questions[i].choices[0]);
-    ans2.html(questions[i].choices[1]);
-    ans3.html(questions[i].choices[2]);
-    ans4.html(questions[i].choices[3]);
-
-    var checkAns = $("<div>");
-    if (ans4 === answer) {
-      checkAns.html("<p>Correct!</p>");
-    } else {
-      checkAns.html("<p>Wrong!</p>");
-    };
-  })
-};
+// };
 
 
 // highScores.on("click", function(event) {
@@ -220,3 +293,18 @@ function gameOverLost() {
   gameOverDiv.html("<h1>" + "Game Over" + "</h1>" + "<p>" + "Score = 0" + "</p>");
   contentDiv.html(gameOverDiv.html());
 }
+
+
+
+
+// var nextQuestion = $(".nextQuestion")
+// // var ansButtons = $(".ansButtons");
+
+// for (var i=0; i<questions.length; i++) {
+//   ans1.on("click", function(event) {
+//     event.preventDefault();
+//     var newQuesDiv = $("<div>");
+//     newQuesDiv.html("<h1>" + questions[i].title + "<h1>" + "<button>" + questions[i].choices[0] + "<br>" + "<button>" + questions[i].choices[1] + "<br>" + "<button>" + questions[i].choices[2] + "<br>" + "<button>" + questions[i].choices[3] + "<br>");
+//     newQuesDiv.html(newQuesDiv.html());
+//   })
+// }
